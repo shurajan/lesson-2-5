ea2d89e17d34d025b202f84399d543a6de5e0e18
alexander_bralnin <alexander.bralnin@gmail.com>
2022-02-10 13:12:44 +0300

    minor fix - added offset

--- src/main/java/com/geekbrains/Main.java
+++ src/main/java/com/geekbrains/Main.java
@@ -9,25 +9,25 @@ public class Main {
         fillArrayTwoThreads();
     }
 
-    private static void fillArrayOneThread(){
+    private static void fillArrayOneThread() {
         int size = 10000000;
         float[] arr = new float[size];
         int h = size / 2;
 
-        for(int i = 0; i< size;i++){
+        for (int i = 0; i < size; i++) {
             arr[i] = 1;
         }
 
         long a = System.currentTimeMillis();
 
-        for(int i = 0; i< size;i++){
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        for (int i = 0; i < size; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
 
         System.out.println("One thread : " + (System.currentTimeMillis() - a));
 
-        for (int i = h-100; i<h+100;i++){
-            System.out.printf(String.valueOf(arr[i]));
+        for (int i = h - 3; i < h + 3; i++) {
+            System.out.printf(String.valueOf(arr[i]) + " ");
         }
 
     }
@@ -39,7 +39,7 @@ public class Main {
         float[] a1 = new float[h];
         float[] a2 = new float[h];
 
-        for(int i = 0; i< size;i++){
+        for (int i = 0; i < size; i++) {
             arr[i] = 1;
         }
 
@@ -48,14 +48,14 @@ public class Main {
         System.arraycopy(arr, 0, a1, 0, h);
         System.arraycopy(arr, h, a2, 0, h);
 
-        Thread runnableThread1 = new Thread(new fillArrayTask(a1));
-        Thread runnableThread2 = new Thread(new fillArrayTask(a2));
+        Thread runnableThread1 = new Thread(new fillArrayTask(a1, 0));
+        Thread runnableThread2 = new Thread(new fillArrayTask(a2, h));
         runnableThread1.start();
         runnableThread2.start();
 
         try {
             runnableThread1.join();
-            runnableThread1.join();
+            runnableThread2.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -65,22 +65,26 @@ public class Main {
 
         System.out.println("Two threads : " + (System.currentTimeMillis() - a));
 
-        for (int i = h-100; i<h+100;i++){
-            System.out.printf(String.valueOf(arr[i]));
+        for (int i = h - 3; i < h + 3; i++) {
+            System.out.printf(String.valueOf(arr[i]) + " ");
         }
 
     }
 
-    public static class fillArrayTask implements Runnable {
+    private static class fillArrayTask implements Runnable {
         private float[] arr;
-        fillArrayTask(float[] arr){
+        private int offset;
+
+        fillArrayTask(float[] arr, int offset) {
             this.arr = arr;
+            this.offset = offset;
         }
 
         @Override
         public void run() {
-            for(int i = 0; i< arr.length;i++){
-                arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = (float) (arr[i] * Math.sin(0.2f + (i + offset) / 5) * Math.cos(0.2f + (i + offset) / 5)
+                        * Math.cos(0.4f + (i + offset) / 2));
             }
         }
     }
